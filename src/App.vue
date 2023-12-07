635962d21c2971008ed07a531eae856bd5eb3073
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2023-12-07 16:20:24 -0500

    Add code INIT

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <TextEditor msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import TextEditor from './components/TextEditor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TextEditor
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
